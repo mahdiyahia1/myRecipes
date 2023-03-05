ba0b49c32a6222992220b3f5a0d5269b5f44df69
Mahdi Yahia <mahdiyahai2001@gmail.com>
2023-03-05 02:08:05 +0200

    project

---
+++ dist/main.js
@@ -0,0 +1,27 @@
+const Source = $("#ingredientContainerTemplate").html()
+const template = Handlebars.compile(Source);
+const recipeElement = $(".recipe-album-container")
+
+const render = function(data) {
+    recipeElement.empty()
+    let newHTML = template(data);
+    recipeElement.append(newHTML)
+}
+
+let favourites = []
+
+$("#ingredient-search-button").on("click", function() {
+    let ingredient = $(this).siblings("#ingredientInputField").val()
+    let glutenCheck = $("#gluten:checked").length // 1 - checked 0 unchecked
+    let dairyCheck = $("#dairy:checked").length
+
+    $.get(`/recipe/${ingredient}?gluten=${glutenCheck}&dairy=${dairyCheck}`)
+    .then((data) => {
+        render(data)
+    })
+})
+
+$(".recipe-album-container").on("click", "img", function() {
+    let firstIngredient = $(this).closest(".recipe-container").find("li:first").text()
+    alert(firstIngredient)
+})
---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const axios = require('axios')
+const INGREDIENTS = 'https://recipes-goodness-elevation.herokuapp.com/recipes/ingredient'
+
+const dairyIngredients = ["Cream","Cheese","Milk","Butter","Creme","Ricotta","Mozzarella","Custard","Cream Cheese"]
+const glutenIngredients = ["Flour","Bread","spaghetti","Biscuits","Beer"]
+let allRecipes = []
+let filteredRecipes = []
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+
+const filterIngredients = function(ingredients, sensitivity) {
+    let flag = 0
+    ingredients.forEach((i) => {
+        sensitivity.forEach((s) => {
+            if(i == s) {
+                flag = 1
+            }
+        })
+    })
+    if (flag == 0) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+app.get('/recipe/:ingredient', function(req, res) {
+    let ingredient = req.params.ingredient
+    let glutenCheck = req.query.gluten
+    let dairyCheck = req.query.dairy
+    allRecipes = []
+    filteredRecipes = []
+    axios.get(`${INGREDIENTS}/${ingredient}`)
+    .then((recipes) => {     
+            recipes.data.results.map((e) => {
+                let recipe = {
+                    id: e.idMeal,
+                    title: e.title,
+                    image: e.thumbnail,
+                    link: e.href,
+                    ingredients: e.ingredients
+                }
+                allRecipes.push(recipe)
+            })
+                if (glutenCheck == 1 && dairyCheck == 0) {
+                    allRecipes.forEach((element) => {
+                        if (filterIngredients(element.ingredients, glutenIngredients)) {
+                            filteredRecipes.push(element)
+                        }
+                    })
+                    res.send(filteredRecipes)
+                    return
+                }
+                if (dairyCheck == 1 && glutenCheck == 0) {
+                    allRecipes.forEach((element) => {
+                        if (filterIngredients(element.ingredients, dairyIngredients)) {
+                            filteredRecipes.push(element)
+                        }
+                    })
+                    res.send(filteredRecipes)
+                    return
+                }
+                if (dairyCheck == 1 && glutenCheck == 1) {
+                    allRecipes.forEach((element) => {
+                        if (filterIngredients(element.ingredients, dairyIngredients) && filterIngredients(element.ingredients, glutenIngredients)) {
+                            filteredRecipes.push(element)
+                        }
+                    })
+                    res.send(filteredRecipes)
+                    return
+                }
+                else {
+                    res.send(allRecipes)
+                    return
+                }
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+})
+
+const port = 3000
+app.listen(port, function () {
+    console.log(`Server running on port ${port}`)
+})
